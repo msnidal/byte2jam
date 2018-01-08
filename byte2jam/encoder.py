f2118f96852147a59788543c0bb43849a15fbe45
Mark Snidal <mark.snidal@gmail.com>
2018-01-08 17:50:45 -0500

    Tidied up decode function

--- byte2jam/encoder.py
+++ byte2jam/encoder.py
@@ -4,6 +4,8 @@ from utils import map_note
 from constants import *
 
 def get_nibble_note(nibble, initial_note, mode):
+    """ Get a note in the sequence from a nibble (half-byte) """
+
     relative_position = nibble >> 1
     is_half_note = nibble & 1
     pitch = map_note(initial_note, mode, relative_position)
@@ -11,7 +13,10 @@ def get_nibble_note(nibble, initial_note, mode):
     return Note(pitch, is_half_note)
 
 def encode(values):
-    """ Encodes values into a MIDI pattern that is returned. Call midi.writemidifile on the pattern to save to disc. """
+    """
+    Encodes values into a MIDI pattern that is returned. Call midi.writemidifile
+    on the pattern to save to disc.
+    """
     # need something to work with
     if type(values) != bytearray:
         return None
@@ -25,8 +30,10 @@ def encode(values):
 
     # get note data from each succeeding byte
     for byte in values[1:]:
-        content_notes.extend([get_nibble_note(byte >> 4, initial_note, mode),
-            get_nibble_note(byte & 15, initial_note, mode)])
+        content_notes.extend([
+            get_nibble_note(byte >> 4, initial_note, mode),
+            get_nibble_note(byte & 15, initial_note, mode)
+            ])
 
     # create note schema from extracted byte data
     schema = ByteJamSchema(initial_note, mode, seq_index, content_notes)
@@ -37,8 +44,6 @@ def decode(pattern):
     """ Decodes values from a well formed MIDI pattern. Returns a bytearray bit string. """
     # extract the padding data
     notes = []
-
-    # don't add non-compliant notes
     for event in pattern[0]:
         if type(event) is midi.events.NoteOffEvent:
             notes.append(Note(event.pitch, event.tick == HALF_NOTE_LENGTH))
@@ -46,38 +51,22 @@ def decode(pattern):
     # get key
     initial_note = notes[0]
 
-    initial_note_match = None
-    for index, schema_initial_note in enumerate(INITIAL_NOTE):
-        if initial_note.pitch == schema_initial_note:
-            initial_note_match = index
-
-    if initial_note_match is None:
-        # no initial note match. 
-        return None
-
-    # flatten out the notes (padding sequence guarantees scale) and match
+    # extract schemas from note sequence: mode from scale and intro sequence
     scale = sorted(notes[1:4] + notes[-4:], key=lambda x: x.pitch)
     mode = map(lambda x: x.get_displacement(initial_note), scale)
-    mode_match = None
-
-    for index, schema_mode in enumerate(MODES):
-        if cmp(mode, schema_mode) == 0:
-            mode_match = index
-
-    if mode_match is None:
-        # not in the schema. 
-        return None
-
-    sequence_match = None
     sequence = map(lambda x: x.modal_position(scale), notes[:4])
 
-    for index, schema_sequence in enumerate(INTRO_SEQUENCE):
-        if cmp(sequence, schema_sequence) == 0:
-            sequence_match = index
+    mode_index = None
+    sequence_index = None
+    initial_note_index = None
 
-    if sequence_match is None:
-        # no intro sequence match. 
+    try:
+        initial_note_index = INITIAL_NOTE.index(initial_note.pitch)
+        mode_index = MODES.index(mode)
+        sequence_index = INTRO_SEQUENCE.index(sequence)
+    except ValueError:
+        # One of the flag criteria do not match the schema layout, invalid sequence
         return None
 
-    schema = ByteJamSchema(initial_note_match, mode_match, sequence_match, notes[4:-4])
+    schema = ByteJamSchema(initial_note_index, mode_index, sequence_index, notes[4:-4])
     return schema.get_bytearray(scale)
